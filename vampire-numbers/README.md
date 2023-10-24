93d56eeefe44d5c63dc4ad9c3a3e25e87aaf2602
pde-bakk <pde-bakk@student.codam.nl>
2023-10-25 01:57:23 +0200

    finally, shoutout to https://stackoverflow.com/questions/17352108/find-all-the-4-digit-vampire-numbers

--- vampire-numbers/README.md
+++ vampire-numbers/README.md
@@ -1,2 +1,7 @@
 # vampire-numbers
 [The problem on code.golf](https://code.golf/vampire-numbers)
+
+Shoutout to 
+[this stackoverflow page](https://stackoverflow.com/questions/17352108/find-all-the-4-digit-vampire-numbers) 
+and [this java playground](https://the-algorithms.com/playground?id=4744) 
+for making me think about the problem differently and allowing me to solve it.
--- vampire-numbers/vampire-numbers.py
+++ vampire-numbers/vampire-numbers.py
@@ -1,56 +1,34 @@
-import collections
 import sys
 
-D={}
-def dtally(xint: int) -> int:
-	xint_backup = xint
-	if xint in D.keys():
-		return D[xint]
-	t = 0
-	# print(f'{xint=}', file=sys.stderr)
-	while xint>0:
-		add = (1<<((xint % 10) * 6))
-		t += add
-		xint //=10
-		# print(f'added {add} so {t = }, new {xint =}', file=sys.stderr)
-	D[xint_backup]=t
-	return t
+def get_digits(num_1: int, num_2: int = 0) -> list[int]:
+	l = []
+	while num_1 > 0:
+		remainder, num_1 = num_1 % 10, num_1 // 10
+		l.append(remainder)
+	while num_2 > 0:
+		remainder, num_2 = num_2 % 10, num_2 // 10
+		l.append(remainder)
+	return sorted(l)
 
-def stally(xint: int):
-	if xint in D:return D[xint]
-	st=sorted(str(xint))
-	D[xint]=st
-	return st
 
-def get_counter(q):
-	if q in D:return D[q]
-	s=str(q)
-	D[q] = collections.Counter(str(q))
-	return D[q]
+def check_fangs(fang_1: int, fang_2: int) -> bool:
+	multiplied = fang_1 * fang_2
+	if fang_1 * 10 <= fang_2 or fang_2 * 10 <= fang_1:
+		return False
+	vampire_digits = get_digits(multiplied)
+	factor_digits = get_digits(fang_1, fang_2)
+	return len(factor_digits)%2==0 and ((fang_1%10)or(fang_2%10)) and vampire_digits == factor_digits
 
-def fangs(x: int) -> int:
-	nd = len(str(x))
-	if nd&1:
-		return 0
-	nd //= 2
-	lo = max(tens[nd-1], (x + tens[nd]-2)//(tens[nd]-1))
-	# hi = int(min(x/lo,x**.5))
+def main():
+	l = set()
+	start, end = 10, 1000
+	for i in range(start, end+1):
+		for j in range(i, end+1):
+			if check_fangs(i, j):
+				print(f'{i*j}, {(i, j)}', file=sys.stderr)
+				l.add(i*j)
+	print(*sorted(l), sep='\n')
 
-	# print(f'{x=}, {lo=}, {hi=}', file=sys.stderr)
 
-	# t=dtally(x)
-	# t=get_counter(x)
-	# print(f'{x=}, dtally(x)={t}', file=sys.stderr)
-	for a in range(max(tens[nd-1], (x + tens[nd]-2)//(tens[nd]-1)), int(min(x/lo,x**.5))+1):
-		b = x//a
-		if a*b==x and ((a%10)or(b%10))and get_counter(x)==get_counter(a)+get_counter(b):
-		# if a*b==x and ((a%10)or(b%10))and t==dtally(a)+dtally(b):
-		# if a*b==x and ((a%10)or(b%10)) and stally(x) == sorted(stally(a)+stally(b)):
-			print(x)
-			return 1
-	return 0
-
-tens = [10**i for i in range(20)]
-for i in range(1_000_000):
-	fangs(i)
-# print(len(D),D,file=sys.stderr)
+if __name__ == '__main__':
+	main()
