b599c9ba8c211566beed70325c506434ec83d351
pde-bakk <pde-bakk@student.codam.nl>
2023-10-25 03:10:47 +0200

    got vampire-numbers.py down to 143 characters

--- vampire-numbers/vampire-numbers.py
+++ vampire-numbers/vampire-numbers.py
@@ -1,14 +1,2 @@
-def F(n,m=0):
-	l=[]
-	while n>0:r,n=n%10,n//10;l.append(r)
-	while m>0:r,m=m%10,m//10;l.append(r)
-	return sorted(l)
-def cf(f1,f2):
-	if f1*10<=f2 or f2*10<=f1:return 0
-	v,f=F(f1*f2),F(f1,f2)
-	return len(f)%2==0 and((f1%10)or(f2%10))and v==f
-l=set()
-for i in range(10,1001):
-	for j in range(i,1001):
-		if cf(i,j):l.add(i*j)
-print(*sorted(l),sep='\n')
+S,R=sorted,range(10,1001)
+print(*S(set(i*j for i in R for j in R if all([len(f:=S(str(i)+str(j)))%2<1,i%10 or j%10,S(str(i*j))==f]))),sep='\n')
